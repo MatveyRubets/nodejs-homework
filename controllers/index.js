b3da8f37023506d8c6d60a79aec99c7f5bea0de8
MatveyRubets <rmatvey11@gmail.com>
2022-10-02 14:18:24 +0200

    init redo for mongoose

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
-const getContacts = require("./getContacts");
-const getContactById = require("./getContactById");
 const addContact = require("./addContact");
-const deleteContact = require("./deleteContact");
-const updateContact = require("./updateContact");
+// const getContacts = require("./getContacts");
+// const getContactById = require("./getContactById");
+// const deleteContact = require("./deleteContact");
+// const updateContact = require("./updateContact");
 
 module.exports = {
-	getContacts,
-	getContactById,
 	addContact,
-	deleteContact,
-	updateContact,
+	// getContacts,
+	// getContactById,
+	// deleteContact,
+	// updateContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,28 +4,28 @@ const controllers = require("../../controllers");
 
 const { controllerWrapper } = require("../../helpers");
 
-const { validateBody } = require("../../middlewares/index");
+// const { validateBody } = require("../../middlewares/index");
 
-const schemas = require("../../schemas/contact");
+// const schemas = require("../../schemas/contact");
 
 const router = express.Router();
 
-router.get("/", controllerWrapper(controllers.getContacts));
+// router.get("/", controllerWrapper(controllers.getContacts));
 
-router.get("/:id", controllerWrapper(controllers.getContactById));
+// router.get("/:id", controllerWrapper(controllers.getContactById));
 
 router.post(
 	"/",
-	validateBody(schemas.addSchema),
+	// validateBody(schemas.addSchema),
 	controllerWrapper(controllers.addContact)
 );
 
-router.delete("/:id", controllerWrapper(controllers.deleteContact));
+// router.delete("/:id", controllerWrapper(controllers.deleteContact));
 
-router.put(
-	"/:id",
-	validateBody(schemas.addSchema),
-	controllerWrapper(controllers.updateContact)
-);
+// router.put(
+// 	"/:id",
+// 	validateBody(schemas.addSchema),
+// 	controllerWrapper(controllers.updateContact)
+// );
 
 module.exports = router;
